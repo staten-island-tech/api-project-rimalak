9df618856ede39b1e01885e314559af263729140
Malak37 <71739461+Malak37@users.noreply.github.com>
2021-05-11 10:00:47 -0400

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,14 @@
 import { DOMSelectors } from "./DOM";
 //import { genres } from "./genre";
 
-/*const query = async function(){
-    try {
-        const response = await fetch(
-            ``
-        )
-    } catch () {
-        
-    }
-}
-*/
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://sports.api.decathlon.com/sports/${id}`
+    );
+    const data = await response.json();
+  } catch (error) {
+    console.log(error);
+    alert("Something got messed up.");
+  }
+};
